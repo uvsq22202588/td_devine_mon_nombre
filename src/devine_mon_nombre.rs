42cf05aa9026fe86cc08fb39e7e0efb53b04c988
Stéphane Lopes <stephane.lopes@uvsq.fr>
2022-12-01 12:16:40 +0100

    Temine la section "lire un nombre"

--- src/devine_mon_nombre.rs
+++ src/devine_mon_nombre.rs
@@ -1,3 +1,9 @@
+use std::io;
+
 fn main() {
-    println!("Hello World!");
+    println!("Devine mon nombre !\n");
+    println!("Saisissez votre proposition.");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    println!("Votre nombre: {}", input.trim());
 }
