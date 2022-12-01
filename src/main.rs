05f0261da8caf070fd109d6c0bc6a9f14da6d5c4
Stéphane Lopes <stephane.lopes@uvsq.fr>
2022-12-01 12:58:38 +0100

    Permet de saisir plusieurs propositions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Devine mon nombre !\n");
@@ -8,17 +8,23 @@ fn main() {
     let secret = rand::thread_rng().gen_range(1..101);
     println!("[DEBUG] Nombre secret : {}", secret);
 
-    println!("Saisissez votre proposition.");
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    let input: u32 = input.trim().parse().unwrap();
-    println!("Votre nombre: {}", input);
-    
-    let message = match input.cmp(&secret) {
-        Ordering::Less => "Trop petit",
-        Ordering::Equal => "Égal",
-        Ordering::Greater => "Trop grand",
-    };
+    loop {
+        println!("Saisissez votre proposition.");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        let input: u32 = input.trim().parse().unwrap();
+        println!("Votre nombre: {}", input);
 
-    println!("{}", message)
+        let message = match input.cmp(&secret) {
+            Ordering::Less => "Trop petit",
+            Ordering::Equal => "Égal",
+            Ordering::Greater => "Trop grand",
+        };
+
+        println!("{}", message);
+
+        if input.cmp(&secret) == Ordering::Equal {
+            break;
+        }
+    }
 }
