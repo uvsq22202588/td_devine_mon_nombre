3451f3443ed6243c2df4615c67af07b8be754e65
Stéphane Lopes <stephane.lopes@uvsq.fr>
2022-12-01 12:42:40 +0100

    Termine la section "Générer un nombre aléatoire"

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Devine mon nombre !\n");
+
+    let secret = rand::thread_rng().gen_range(1..101);
+    println!("[DEBUG] Nombre secret : {}", secret);
+
     println!("Saisissez votre proposition.");
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
