37ec127aed56a2fd685c9a745f8d22e3a446ee80
uvsq22202588 <lenny.lemoussu@ens.uvsq.fr>
2025-09-08 19:29:20 +0200

    Comparer le nombre saison avec le nombre généré,4

--- src/src/main.rs
+++ src/src/main.rs
@@ -1,16 +1,27 @@
 use std::io;
-use rand;
+use rand::Rng;
 
 fn main() {
     println!("Devine mon nombre !\n");
-    println!("Saississez votre proposition.");
+    println!("Saisissez votre proposition.\n");
 
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
-    
-    println!("Le nombre que vous avez dit est : {}", input);
+    let input_int: u32 = input.trim().parse().unwrap();
+
+    println!("Le nombre que vous avez dit est : {}", input_int);
 
     let variable_aleatoire = rand::thread_rng().gen_range(1..101);
 
-    println!("Le nombre aléatoire est : {}", variable_aleatoire)
+    println!("Le nombre aléatoire est : {}", variable_aleatoire);
+
+    let message = if input_int == variable_aleatoire {
+        "Tu as choisi le bon numéro !"
+    } else if input_int < variable_aleatoire {
+        "Tu as choisi un numéro trop petit !"
+    } else {
+        "Tu as choisi un numéro trop grand !"
+    };
+
+    println!("{}", message);
 }
