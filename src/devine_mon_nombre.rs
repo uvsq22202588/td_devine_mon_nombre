21b0971714ca0beb5b19202f4ed4a1acdbc58dfc
uvsq22202588 <lenny.lemoussu@ens.uvsq.fr>
2025-09-08 18:37:24 +0200

    Q4

--- src/devine_mon_nombre.rs
+++ src/devine_mon_nombre.rs
@@ -1,4 +1,11 @@
+use std::io;
+
 fn main() {
     println!("Devine mon nombre !\n");
     println!("Saississez votre proposition.");
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input);
+    
+    println!("Le nombre que vous avez dit est {}", input)
 }
